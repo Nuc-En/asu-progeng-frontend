d59d5f13fd04f8d34d5369b7e6570461c971ab39
d.bublikov <d.bublikov@estesis.tech>
2025-12-15 16:35:43 +0700

    style(products): центровка грида

--- src/pages/products/index.vue
+++ src/pages/products/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import ProductCard from '@/components/ProductCard.vue';
-import { productMock } from '@/mock/ProductMock';
+import ProductCard from "@/components/ProductCard.vue";
+import { productMock } from "@/mock/ProductMock";
 </script>
 
 <template>
-  <div class="flex flex-wrap gap-4 container mx-auto">
+  <div class="flex flex-wrap items-center gap-4 container mx-auto">
     <ProductCard :product v-for="product in productMock" :key="product.id" />
   </div>
 </template>
